7b9dabf2c80cb93107563efd3303f29c65e28fd1
NikolaCredeo <n.milosevic0111@gmail.com>
2024-02-01 16:01:05 +0100

    removing app bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,8 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:kiwi/kiwi.dart';
 import 'package:shopengo/core/services/environment_service.dart';
 import 'package:shopengo/core/services/router_service.dart';
-import 'package:shopengo/features/app_bloc/app_bloc.dart';
-import 'package:shopengo/features/app_bloc/app_event.dart';
-import 'package:shopengo/features/app_bloc/app_state.dart';
 import 'package:shopengo/kiwi_container.dart';
 
 void main() async {
@@ -24,38 +20,21 @@ class Shopengo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => AppBloc()..add(LoadApp()),
-      child: BlocBuilder<AppBloc, AppState>(
-        builder: (context, state) {
-          if (state is AppInitial) {
-            // show splash screen here if needed
-            return const SizedBox.shrink();
-          }
-
-          if (state is AppLoaded) {
-            // we can get theme and other app settings from app bloc
-            final router = KiwiContainer().resolve<RouterService>().router;
-            return EasyLocalization(
-              supportedLocales: const [Locale('sr'), Locale('en')],
-              fallbackLocale: const Locale('en'),
-              path: 'assets/localization',
-              child: Builder(
-                builder: (context) {
-                  return MaterialApp.router(
-                    title: 'Shopengo',
-                    routerConfig: router,
-                    locale: context.locale,
-                    supportedLocales: context.supportedLocales,
-                    localizationsDelegates: context.localizationDelegates,
-                    debugShowCheckedModeBanner: false,
-                  );
-                },
-              ),
-            );
-          }
-
-          return const SizedBox.shrink();
+    final router = KiwiContainer().resolve<RouterService>().router;
+    return EasyLocalization(
+      supportedLocales: const [Locale('sr'), Locale('en')],
+      fallbackLocale: const Locale('en'),
+      path: 'assets/localization',
+      child: Builder(
+        builder: (context) {
+          return MaterialApp.router(
+            title: 'Shopengo',
+            routerConfig: router,
+            locale: context.locale,
+            supportedLocales: context.supportedLocales,
+            localizationsDelegates: context.localizationDelegates,
+            debugShowCheckedModeBanner: false,
+          );
         },
       ),
     );
