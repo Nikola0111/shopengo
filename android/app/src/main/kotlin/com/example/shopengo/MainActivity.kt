c7fcdea537d894f95df9c01c0c4c1a7d5b9c2ef3
vaneleey <n.milosevic0111@gmail.com>
2025-04-15 21:32:02 +0200

    recreated project with new flutter version

---
+++ android/app/src/main/kotlin/com/example/shopengo/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.shopengo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- android/app/src/main/kotlin/com/shopengo/shopengo/shopengo/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.shopengo.shopengo.shopengo
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
